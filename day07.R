c32981cd45baf8299d24b52802c56d9e48cb3b08
anhinton <ashley@canadia.co.nz>
2025-12-08 11:59:27 +1300

    day 7 part 2

--- day07.R
+++ day07.R
@@ -1,3 +1,5 @@
+options(scipen = 9)
+
 test = ".......S.......
 ...............
 .......^.......
@@ -56,6 +58,8 @@ count_splits(input)
 
 ## Part 2 ----
 
+x = test
+
 count_timelines = function(x) {
   diagram_matrix = x |>
     lapply(
@@ -67,32 +71,34 @@ count_timelines = function(x) {
       nrow = length(x)
       , byrow = TRUE
     )
-  # paths = 0
+  paths_matrix = matrix(
+    data = 0
+    , nrow = nrow(diagram_matrix)
+    , ncol = ncol(diagram_matrix)
+  )
+  split = 0
   for(i in 2:nrow(diagram_matrix)) {
     for (j in 1:ncol(diagram_matrix)) {
-      if (diagram_matrix[i - 1, j] %in% c("S", "|")) {
-        if (diagram_matrix[i, j] == ".") {
+      if (diagram_matrix[i - 1, j] == "S") {
+        diagram_matrix[i, j] = "|"
+        paths_matrix[i, j] = 1
+      } else if (diagram_matrix[i - 1, j]  == "|") {
+        if (diagram_matrix[i, j] %in% c(".", "|")) {
           diagram_matrix[i, j] = "|"
-        } else if (diagram_matrix[i, j] == "^" ) {
+          paths_matrix[i, j] = paths_matrix[i, j] + paths_matrix[i - 1, j]
+        } else if (diagram_matrix[i, j] == "^") {
           this_row = c(j - 1, j + 1)
-          diagram_matrix[i, sample(this_row, size = 1)] = "|"
-          # paths = paths + 2
+          this_row = this_row[this_row >= 0 & this_row <= ncol(diagram_matrix)]
+          diagram_matrix[i, this_row] = "|"
+          paths_matrix[i, this_row] = paths_matrix[i, this_row] + paths_matrix[i - 1, j]
+          split = split + 1
         }
       }
     }
-    # if (i %% 2 == 0) {
-    #   cat(
-    #     apply(diagram_matrix, MAR = 1, FUN = paste, collapse = "")
-    #     , sep = "\n"
-    #   )
-    #   print(paste("i:", i, "; paths:", paths))
-    # }
   }
-  # paths
-  cat(
-    apply(diagram_matrix, MAR = 1, FUN = paste, collapse = "")
-    , sep = "\n"
-  )
+  sum(paths_matrix[nrow(paths_matrix), ])
 }
 
-count_timelines(test)
+count_timelines(test) == 40
+
+count_timelines(input)
